0827e1e511f10d4638c90609d6ee15fd9afecdc9
Darren Jaworski <darrenjaworski@gmail.com>
2025-08-10 03:27:10 -0500

    ci: add gh pages deployment on successful static checks

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
+const config = defineConfig({
   plugins: [react()],
 });
+
+if (process?.env?.PUBLIC_URL) {
+  config.base = process.env.PUBLIC_URL;
+}
+
+export default config;
