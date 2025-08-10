15b2c561775df7798c6f1d7cc8f348df4e451d67
Darren Jaworski <darrenjaworski@gmail.com>
2025-08-10 12:39:53 -0500

    style: span entire height of page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Header } from "./components/Header";
 
 function App() {
   return (
-    <div className="min-h-screen bg-bg-light text-text-light dark:bg-bg-dark dark:text-text-dark transition-colors">
+    <div className="min-h-screen flex flex-col bg-bg-light text-text-light dark:bg-bg-dark dark:text-text-dark transition-colors">
       <Header />
-      <main className="mx-auto px-4 py-6">
+      <main className="mx-auto px-4 py-6 flex-1 min-h-0">
         <Board />
       </main>
     </div>
