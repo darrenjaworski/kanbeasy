1ab68b447bb32e028c8cd597b90d2f59840c7a83
Darren Jaworski <darrenjaworski@gmail.com>
2025-08-13 00:45:15 -0500

    chore: update coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,5 +8,18 @@ export default defineConfig({
     globals: true,
     setupFiles: ["./src/test/setup.ts"],
     include: ["src/**/*.{test,spec}.{ts,tsx}"],
+    coverage: {
+      provider: "v8",
+      exclude: [
+        "playwright-report/**",
+        "playwright.config.{ts,js,mjs,cjs}",
+        "tests-e2e/**",
+        "eslint.config.js",
+        "vite.config.ts",
+        "vitest.config.ts",
+        "dist/**",
+        "vite-env.d.ts",
+      ],
+    },
   },
 });
